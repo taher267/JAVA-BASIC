c34e478d84c0b9fde79978f6021ae4f233b3f457
ABU TAHER <abutaher267@gmail.com>
2023-08-22 20:21:25 +0600

    first commit

---
+++ Assignment_1.java
@@ -0,0 +1,16 @@
+public class Assignment_1 {
+    public static void main(String[] args) {
+        System.out.println("id: 101,");
+        System.out.println("title: iphone15,");
+        System.out.println("price: 1895 euros,");
+        System.out.println("description: perfect product with beest image quality,");
+        System.out.println("category: phone,");
+
+    }
+}
+
+// public class Main {
+// public static void main(String[] args) {
+// System.out.println("Hello World");
+// }
+// }
